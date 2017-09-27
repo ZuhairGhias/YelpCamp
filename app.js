d457a87845da072c91f309a737290d943caf17f3
Zuhair <zuhair9999aamer@gmail.com>
2017-09-27 23:31:03 +0000

    added mongolab url

--- app.js
+++ app.js
@@ -10,7 +10,8 @@ var indexRoutes = require("./routes/index");
 var campgroundRoutes = require("./routes/campgrounds");
 var commentRoutes = require("./routes/comments");
 
-mongoose.connect("mongodb://localhost/yelp_camp");
+//mongoose.connect("mongodb://localhost/yelp_camp");
+mongoose.connect("mongodb://ghiaszuh:ghiaszuh@ds155424.mlab.com:55424/yelpcamp");
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static(__dirname+ "/public")); // Allows us to use scripts and css files in public
@@ -40,7 +41,6 @@ app.use(function(req, res, next){
 app.use(indexRoutes);
 app.use("/campgrounds", campgroundRoutes);
 app.use("/campgrounds/:id/comments", commentRoutes);
-
 
 app.listen(process.env.PORT, process.env.IP, function(){
     console.log("The YelpCamp server is running...");
